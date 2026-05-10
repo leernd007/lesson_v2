245c82447ab78ff28cdc6534a6eb9b82aedfd8de
leernd007 <60017624+leernd007@users.noreply.github.com>
2026-05-10 14:53:37 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(data):
+
+    username = data['username']
+    message = data['message']
+
+    full_message = f"{username}: {message}"
+
+    send(full_message, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
